a318eaedf9568a1ed1d5bd41a3b56255a0b54813
Nikola_Antonov <Android2.1666@email.com>
2023-07-07 14:29:59 +0500

    added app,token,user,dlc,initDatabase

---
+++ server/startUp/initDatabase.js
@@ -0,0 +1,29 @@
+
+
+
+
+const Dlc = require('../models/Dlc')
+const dlcMock = require('../mock/dlc.json')
+
+module.exports = async () => {
+const Dlcs = await Dlc.find()
+    if(Dlcs.length !== dlcMock.length){
+      await  createInitialEntity(Dlc, dlcMock)
+    }
+}
+
+async function createInitialEntity(Model, data){
+    await Model.collection.drop()
+    return Promise.all(
+        data.map(async item => {
+            try{
+                delete  item._id
+                const newItem = new Model(item)
+                await newItem.save()
+                return newItem
+            }catch (e){
+                return e
+            }
+        })
+    )
+}
