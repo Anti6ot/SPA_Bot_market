dc6507804213a0c276241e1067ba87dfdc551eab
Anti6ot <kradenihs@gmail.com>
2023-07-08 13:09:31 +0500

    created models-comment, created routes-auth,comment e.t.c

---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router({mergeParams: true})
+
+router.use('/auth', require('./auth.routes'))
+router.use('/user', require('./user.routes'))
+router.use('/dlc', require('./dlc.routes'))
+router.use('/comment', require('./comment.routes'))
+
+
+
+
+module.exports = router
